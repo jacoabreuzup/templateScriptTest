cfc670078cf3d66caac34c32e16789e81af41c59
jacoabreuzup <joao.abreu@zup.com.br>
2021-05-14 16:53:09 -0300

    first commit

---
+++ ios-templates.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# This script will create Beagle templates for you to use in Xcode.
+
+TEMPLATES_DIRECTORY="/Users/$1/Library/Developer/Xcode/Templates"
+
+if [ ! -d $TEMPLATES_DIRECTORY ]
+then
+    mkdir $TEMPLATES_DIRECTORY
+    cd $TEMPLATES_DIRECTORY
+    git clone https://github.com/ZupIT/beagle-ios-templates.git
+    echo "Templates created."
+fi
+
+exit 0
